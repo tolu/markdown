2810edfe778298939a1751e6a1e4832fe3529ec2
Tobias Lundin <tobias.lundin@gmail.com>
2016-11-15 09:38:23 +0100

    fix indentation

--- WebRequester.cs
+++ WebRequester.cs
@@ -2,41 +2,39 @@
 
 namespace An.Example
 {
-
 	public class MyController : Controller
 	{
-
 		public async Task<ActionResult> MyRequest(string id)
 		{
 			var accessToken = GetCurrentUserAccessToken();
 			var json = await ApiClient.Fetch<EpisodeVm>($"mediaelement/{id}", accessToken);
-      return Json(json);
+			return Json(json);
 		}
-    
+
 		private string GetCurrentUserAccessToken()
 		{
-      var currentUser = IoC.UserProvider().GetCurrentUser();
-      return currentUser == null
-          ? string.Empty
-          : currentUser.AccessToken.Raw;
+			var currentUser = IoC.UserProvider().GetCurrentUser();
+			return currentUser == null
+				? string.Empty
+				: currentUser.AccessToken.Raw;
 		}
 	}
 
 	public class ApiClient
 	{
 		private static readonly HttpClient Client;
-    private static readonly string ApiKey;
+		private static readonly string ApiKey;
 
 		static ApiClient()
 		{
 			var apiBasePath = IoC.Settings().ApiBaseUrl;
 			Client = CreateHttpClient(apiBasePath);
-		    ApiKey = IoC.Settings().ApiKey;
+			ApiKey = IoC.Settings().ApiKey;
 		}
 
 		public static async Task<T> Fetch<T>(string url, string accessToken) where T : class
 		{
-      var urlWithApiKey = AddApiKeyToUrl(url);
+			var urlWithApiKey = AddApiKeyToUrl(url);
 			var response = await Client.SendAsync(GetRequestMessage(accessToken, urlWithApiKey));
 			var warning = response.Headers.Warning as IEnumerable<WarningHeaderValue>;
 			if (!response.IsSuccessStatusCode)
@@ -51,13 +49,13 @@ namespace An.Example
 
 	    private static string AddApiKeyToUrl(string url)
 	    {
-        if (url.Contains("?"))
-        {
-            return url + "&apikey=" + ApiKey;
-        }
+			if (url.Contains("?"))
+			{
+				return url + "&apikey=" + ApiKey;
+			}
 
-        return url + "?apikey=" + ApiKey;
-      }
+			return url + "?apikey=" + ApiKey;
+      	}
 
 		private static HttpClient CreateHttpClient(string apiBasePath)
 		{
